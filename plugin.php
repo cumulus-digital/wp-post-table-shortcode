7439c044da06cf4ca46319bd97b8731ab525f28d
Daniel Vena <deaconvena+github@gmail.com>
2021-06-08 16:24:38 -0400

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,70 @@
+<?php
+namespace CUMULUS\Wordpress\PostsTableShortcode;
+/**
+ * Plugin Name: Post Table Shortcode
+ * Plugin URI: https://github.com/cumulus-digital/wp-post-table-shortcode/
+ * Description: Provides a Shortcode for including a simple table of posts
+ * Version: 0.1
+ * Author: vena
+ * License: UNLICENSED
+ * GitHub Plugin URI: cumulus-digital/wp-post-table-shortcode
+ */
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+const PLUGIN_NAME = 'wp-post-table-shortcode';
+const PREFIX = 'wp-post-table-shortcode';
+const TXTDOMAIN = PREFIX;
+const BASEPATH = PLUGIN_NAME;
+const BASE_FILENAME = PLUGIN_NAME . DIRECTORY_SEPARATOR . 'plugin.php';
+
+function post_table_shortcode($attr) {
+    $attr = \shortcode_atts([
+        'category' => null,
+        'tag' => null,
+        'order' => 'asc',
+        'orderby' => 'date',
+        'before' => null,
+        'after' => date('Y') - 1,
+        'max' => -1,
+        'date_format' => 'm/d/y'
+    ], $attr, 'post_table');
+
+    $posts = (new \WP_Query([
+        'posts_per_page' => $attr['max'],
+        'orderby' => $attr['orderby'],
+        'order' => $attr['order'],
+        'category_name' => $attr['category'],
+        'date_query' => array(
+            array(
+                'after' => $attr['after'],
+                'before' => $attr['before']
+            )
+        )
+    ]));
+
+    ob_start();
+?>
+    <figure class="wp-block-table is-style-stripes">
+        <table>
+            <tbody>
+                <?php while($posts->have_posts()): $posts->the_post(); ?>
+                <tr>
+                    <td class="has-text-align-right" data-align="right">
+                        <?php \the_date($attr['date_format']); ?>
+                    </td>
+                    <td>
+                        <?php the_title( sprintf( '<a href="%s">', \esc_url( \get_permalink() ) ), '</a>' ); ?>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </figure>
+<?php
+    $output = ob_get_clean();
+    \wp_reset_postdata();
+
+    return $output;
+}
+\add_shortcode('posts-table', __NAMESPACE__ . '\\post_table_shortcode');
